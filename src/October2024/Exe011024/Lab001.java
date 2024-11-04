21de3ef0736f251662621b951ff0eadd780b2245
Nafiha <nafinalakath666@gmail.com>
2024-11-04 20:33:39 +0530

    Initial commit

---
+++ src/October2024/Exe011024/Lab001.java
@@ -0,0 +1,8 @@
+package October2024.Exe011024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("hi");
+    }
+}
---
+++ src/Task/Oct01/callculator.java
@@ -0,0 +1,11 @@
+package Task.Oct01;
+
+public class callculator {
+    public static void main(String[] args) {
+        System.out.println("Simple calculator with Numbers 10 and 3 ");
+        System.out.println(10 + 3);
+        System.out.println(10-3);
+        System.out.println(10*3);
+        System.out.println(10/3);
+    }
+}
